0104664cfbba3268e467bf8fb7829d900a9e6fb0
Mancilladmm7 <mancilla.dmm@gmail.com>
2025-07-15 00:16:28 -0600

    Add files via upload

--- app.js
+++ app.js
@@ -212,6 +212,11 @@ function actualizarSelect() {
   });
 }
 
+function convertirABase64(file, callback) {
+  const reader = new FileReader();
+  reader.onloadend = () => callback(reader.result);
+  reader.readAsDataURL(file);
+}
 
 // Inicializar
 actualizarSelect();
